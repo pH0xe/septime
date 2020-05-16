005b296b2720c04f8fae6cca0c4a1e8fbc751ef6
pH0xe <reig.julien@free.fr>
2020-05-16 16:20:58 +0200

    adding tab ffe in news section

--- src/pages/NewsPage.vue
+++ src/pages/NewsPage.vue
@@ -2,26 +2,84 @@
   <q-page>
     <div class="row justify-center q-mx-sm">
       <div class="page-padded col-12 col-md-8">
-        <div class="text-h5 q-mb-md">
-          Actualités
-        </div>
-        <q-separator class="separator-margin" />
+        <q-splitter
+          v-model="newsTab"
+          :limits="[10, 10]"
+        >
+          <template v-slot:before>
+            <q-tabs
+              v-model="tab"
+              vertical
+              class="text-primary"
+            >
+              <q-tab
+                name="news"
+                icon="mdi-newspaper"
+                label="Club"
+              />
+              <q-tab
+                name="ffe"
+                icon="mdi-fencing"
+                label="FFE"
+              />
+            </q-tabs>
+          </template>
 
-        <div class="row q-col-gutter-md">
-          <div
-            v-for="news in sortedNews"
-            :key="news.uid"
-            class="col-12 col-md-4"
-            @click.prevent="onClickNews(news.uid)"
-          >
-            <news-card
-              :title="news.title"
-              :content="news.text"
-              :date="news.date"
-              :img-src="news.imgURL"
-            />
-          </div>
-        </div>
+          <template v-slot:after>
+            <q-tab-panels
+              v-model="tab"
+              animated
+              vertical
+              transition-prev="jump-up"
+              transition-next="jump-up"
+            >
+              <q-tab-panel name="news">
+                <div class="text-h5 q-mb-md">
+                  Actualités du club
+                </div>
+                <q-separator class="separator-margin" />
+
+                <div class="row q-col-gutter-md">
+                  <div
+                    v-for="news in sortedNews"
+                    :key="news.uid"
+                    class="col-12 col-md-4"
+                    @click.prevent="onClickNews(news.uid)"
+                  >
+                    <news-card
+                      :title="news.title"
+                      :content="news.text"
+                      :date="news.date"
+                      :img-src="news.imgURL"
+                    />
+                  </div>
+                </div>
+              </q-tab-panel>
+
+              <q-tab-panel name="ffe">
+                <div class="text-h5 q-mb-md">
+                  Actualités FFE
+                </div>
+                <q-separator class="separator-margin" />
+                <div class="row q-col-gutter-md">
+                  <div
+                    v-for="news in sortedNewsFFE"
+                    :key="news.uid"
+                    class="col-12 col-md-4"
+                    @click.prevent="onClickNews(news.uid)"
+                  >
+                    <news-card
+                      :title="news.title"
+                      :content="news.text"
+                      :date="news.date"
+                      :img-src="news.imgURL"
+                    />
+                  </div>
+                </div>
+              </q-tab-panel>
+            </q-tab-panels>
+          </template>
+        </q-splitter>
       </div>
     </div>
   </q-page>
@@ -34,13 +92,32 @@ import NewsCard from '../components/NewsCard';
 export default {
   name: 'NewsPage',
   components: { NewsCard },
+
+  data: () => ({
+    newsTab: 10,
+    tab: 'news'
+  }),
+
   computed: {
     ...mapState({
-      news: (state) => state.news.news
+      news: (state) => state.news.news,
+      ffeInfo: (state) => state.news.ffeInfo
     }),
 
     sortedNews() {
       const newsCopy = Array.from(this.news);
+      newsCopy.sort((news1, news2) => {
+        if (news1.date > news2.date) {
+          return -1;
+        }
+        return 1;
+      });
+
+      return newsCopy;
+    },
+
+    sortedNewsFFE() {
+      const newsCopy = Array.from(this.ffeInfo);
       newsCopy.sort((news1, news2) => {
         if (news1.date > news2.date) {
           return -1;
